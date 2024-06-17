9547f86cb143f3f16cbcb4b67b1f7e04dfd0478b
abdulhp <ahp.abdul@gmail.com>
2024-06-17 10:42:12 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/scripts/views/pages/tentang-kita.js
@@ -0,0 +1,12 @@
+const tentangKita = {
+  async render() {
+    return `
+          <h2> Ini Adalah halaman Tentang Kita</h2>
+          
+          `;
+  },
+
+  async afterRender() { },
+};
+
+export default tentangKita;
